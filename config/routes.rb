3779f2d25dd015c93681009f1cb47cb89bfee1ef
Arthur Littmann <arthur@lewagon.org>
2020-05-11 17:59:31 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "flats#home"
+  # localhost:3000/flats
+  get '/flats', to: "flats#index", as: :flats
+
+  # localhost:3000/flats/12312
+  get '/flats/:id', to: 'flats#show', as: :flat
 end
